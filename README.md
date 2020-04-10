d0622c5e8e3b49bf5ea5d3333e2144bc5fe88055
gphgjcmqgg <gphgjcmqgg@sina.com>
2020-04-10 23:05:04 +0800

    无状态组件

--- README.md
+++ README.md
@@ -45,4 +45,14 @@ https://www.bootcdn.cn/
 npm config set registry https://registry.npm.taobao.org
 yarn config set registry https://registry.npm.taobao.org
 ```
+## react组件创建方式
 
+1. createClass
+2. class X extends React.Component
+3. const X = (props) => { return (......)}
+创建函数式组件（无状态组件）
+1） 无需state，即不处理用户的输入，组件的所有的数据都是依赖props传入的
+2）不需要用到生命周期函数
+好处
+1）不需要声明类，可以避免大量的譬如extends或者constructor这样的代码
+2）不需要显示声明this关键字，在ES6的类声明中往往需要将函数的this关键字绑定到当前作用域，而因为函数式声明的特性，我们不需要再强制绑定
---
+++ hello-react/src/components/Home.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: props.initialAge,
+    }
+  }
+
+  onMakeOlder() {
+    this.setState({
+      age: this.state.age + 3
+    })
+  }
+
+  handleGreet() {
+    this.props.greet(this.state.age)
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <div>your name is {this.props.name}, your age is {this.state.age}</div>
+            <button onClick={() => {this.onMakeOlder()}} className="btn btn-primary">Make me older</button>
+            <hr />
+            <button onClick={this.handleGreet.bind(this)} className="btn btn-primary">Greet</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  user: PropTypes.object,
+  greet: PropTypes.func
+};
